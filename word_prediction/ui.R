1329d2534411ac419413d1602e3e6764568a1a06
Cogabi <gabiconst22@gmail.com>
2021-12-29 01:18:37 +0200

    Second commit of application

--- word_prediction/ui.R
+++ word_prediction/ui.R
@@ -3,12 +3,16 @@ library(shinyjs)
 
 shinyUI(fluidPage(
     shinyjs::useShinyjs(),
-    titlePanel(div("Coursera Data Science Capstone - Word Prediction Application", align = "center")),
+    tags$div(
+    titlePanel(div(p("Coursera Data Science Capstone - Word Prediction Application", align = "center", style= "color: white"),
+                   style = "background-color: #1f2378; width: 100%; height: 40px")),
+    
     mainPanel(style = "margin-left:150px",
-              p(strong("Instructions"), align = "center", style = "font-size: 20px; margin-top: 80px"),
-              p(strong("1. Enter the first word in the text box below."), align = "center", style = "font-size: 20px"),
-              p(strong("2. Wait a few seconds for the dictionaries to load."), align = "center", style = "font-size: 20px"),
+              p(strong("Instructions:"), align = "center", style = "font-size: 20px; margin-top: 80px"),
+              p(strong("1. Enter the first word in the text box below;"), align = "center", style = "font-size: 20px"),
+              p(strong("2. Wait a few seconds for the dictionaries to load;"), align = "center", style = "font-size: 20px"),
               p(strong("3. Add a new word to the text by just clicking on it."), align = "center", style = "font-size: 20px"),
+            
               tags$div(align = "center", 
                        textInput("text", label = "", width = '85%'),
                        tags$style("#text {font-size: 20px}"),
@@ -16,9 +20,21 @@ shinyUI(fluidPage(
                        uiOutput("wordTwoButton", inline = TRUE),
                        uiOutput("wordThreeButton", inline = TRUE),
                        uiOutput("wordFourButton", inline = TRUE),
-                       uiOutput("wordFiveButton", inline = TRUE)
+                       uiOutput("wordFiveButton", inline = TRUE),
+                       
+         
+                       p("Important information:", align = "center", style = "font-size: 20px; margin-top: 80px"),
+                       tags$ul(
+                         tags$li("The model does not use any stopwords such as: the, a, not, so, what, i, me, etc.;", align = "left"), 
+                         tags$li("The model does not check for spelling errors;", align = "left"), 
+                         tags$li("If a word is not recognized by the model, the output will be NA. Simply delete the word and try another.", align = "left")
+                       ),
+                   
+                       tags$footer(p("Model built on HC Corpora provided by Swiftkey", style = "margin-top: 20px; color: white", align = "center"), 
+                                   style = "position: absoulte; bottom:0; width: 100%; height: 30px; background-color: #1f2378")
+                    
               )
+    )  
     )
-    
-    
+
 ))
